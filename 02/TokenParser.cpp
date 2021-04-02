697cbb490d9a977a04f43411ddf5d9dc11299259
Nelly <57725408+nelly125@users.noreply.github.com>
2021-04-02 18:21:22 +0300

    Add files via upload

--- 02/TokenParser.cpp
+++ 02/TokenParser.cpp
@@ -7,10 +7,10 @@
 #include "TokenParser.hpp"
 
 TokenParser::TokenParser() :
-	StartCallback(nullptr),
-	FinishCallback(nullptr),
-	DigitTokenCallback(nullptr),
-	StringTokenCallback(nullptr) {}
+  StartCallback(nullptr),
+  FinishCallback(nullptr),
+  DigitTokenCallback(nullptr),
+  StringTokenCallback(nullptr) {}
 
 TokenParser::~TokenParser() = default;
 
@@ -23,19 +23,19 @@ void TokenParser::SetEndCallback(const std::function<void()> &finish_callback) {
 }
 
 void TokenParser::SetDigitTokenCallback(const std::function<void(const uint64_t)>
-	       	&digit_callback) {
+                                        &digit_callback) {
   DigitTokenCallback = digit_callback;
 }
 
 void TokenParser::SetStringTokenCallback(const std::function<void(const std::string &)>
-	       	&string_callback) {
+                                         &string_callback) {
   StringTokenCallback = string_callback;
 }
 
 bool TokenParser::IsDigit(const std::string &str) {
   for (char i : str) {
-	if (!isdigit(i))
-	  return false;
+    if (!isdigit(i))
+      return false;
   }
   return true;
 }
@@ -68,7 +68,7 @@ void TokenParser::Type_of_token(const std::string &temp_token) {
 
 void TokenParser::Parser(const std::string &str) {
   if (StartCallback != nullptr)
-	StartCallback();
+    StartCallback();
   std::string token;
   std::string delimiter = " \t\n";
 
@@ -76,19 +76,18 @@ void TokenParser::Parser(const std::string &str) {
   pos = str.find_first_of(delimiter);
 
   while (pos != std::string::npos) {
-	if (begin == pos) {
-	  begin = pos + 1;
-	} else {
-	  token = str.substr(begin, pos - begin);
-	  begin = pos + 1;
-	  Type_of_token(token);
-	}
-	pos = str.find_first_of(delimiter, begin);
+    if (begin == pos) {
+      begin = pos + 1;
+    } else {
+      token = str.substr(begin, pos - begin);
+      begin = pos + 1;
+      Type_of_token(token);
+    }
+    pos = str.find_first_of(delimiter, begin);
   }
   token = str.substr(begin, pos - begin);
   if (!token.empty())
-	Type_of_token(token);
-
+    Type_of_token(token);
   if (FinishCallback != nullptr)
-	FinishCallback();
+    FinishCallback();
 }
